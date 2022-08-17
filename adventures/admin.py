8b10ec6aee272318d76ccdef2eebd69ee537710a
ConRdav <connor@redwoodglade.com>
2022-08-17 15:36:21 +0000

    customise admin

--- adventures/admin.py
+++ adventures/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Adventure, Category
 
 # Register your models here.
-admin.site.register(Adventure)
-admin.site.register(Category)
+
+class AdventureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'country',
+        'continent',
+        'theme',
+        'image',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+admin.site.register(Adventure, AdventureAdmin)
+admin.site.register(Category, CategoryAdmin)
+
