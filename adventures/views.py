08f9d72391073b8c1954436cb0f480bfac260b4e
ConRdav <connor@redwoodglade.com>
2022-08-17 15:46:47 +0000

    Add adventure views and templates

--- adventures/views.py
+++ adventures/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Adventure
 
 # Create your views here.
+def all_adventures(request):
+    """ A view to show all adventures, including sorting and search queries """
+
+    adventures = Adventure.objects.all()
+
+    context = {
+        'adventures': adventures,
+    }
+
+    return render(request, 'adventures/adventures.html', context)
