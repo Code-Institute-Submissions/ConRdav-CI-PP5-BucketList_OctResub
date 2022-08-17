ab54bd3b392ef95689b6240999b98f4461323f5e
ConRdav <connor@redwoodglade.com>
2022-08-17 17:10:51 +0000

    Add adventure details functionality

--- adventures/views.py
+++ adventures/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Adventure
 
 # Create your views here.
@@ -9,6 +9,18 @@ def all_adventures(request):
 
     context = {
         'adventures': adventures,
+    }
+
+    return render(request, 'adventures/adventures.html', context)
+
+
+def adventure_detail(request, adventure_id):
+    """ A view to show adventure details """
+
+    adventure = get_object_or_404(Adventure, pk=adventure_id)
+
+    context = {
+        'adventure': adventure,
     }
 
     return render(request, 'adventures/adventures.html', context)
