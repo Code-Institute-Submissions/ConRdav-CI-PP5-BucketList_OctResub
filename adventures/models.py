e694ae3f590e842cff904e5e816d9e346b4825b0
ConRdav <connor@redwoodglade.com>
2022-08-17 15:17:46 +0000

    add adventures app, adventure model and category model

---
+++ adventures/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Adventure(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    country = models.CharField(max_length=254)
+    continent = models.CharField(max_length=254)
+    theme = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
