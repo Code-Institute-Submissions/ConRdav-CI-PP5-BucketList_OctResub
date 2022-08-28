4e2234fdcce282494b9adbc7044aa46aa4a3a6a8
ConRdav <connor@redwoodglade.com>
2022-08-28 20:08:47 +0000

    Added category filtering

--- adventures/views.py
+++ adventures/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Adventure
+from .models import Adventure, Category
 
 # Create your views here.
+
+
 def all_adventures(request):
     """ A view to show all adventures, including sorting and search queries """
 
     adventures = Adventure.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            adventures = adventures.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'adventures': adventures,
+        'current_categories': categories,
     }
 
     return render(request, 'adventures/adventures.html', context)
