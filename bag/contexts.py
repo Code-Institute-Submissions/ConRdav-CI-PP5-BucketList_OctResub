9eb36bc017460cc9238eb94478a3d768bce9f8a4
ConRdav <connor@redwoodglade.com>
2022-08-28 21:46:37 +0000

    update context processor to show shopping bag contents and price

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,24 @@
-from decimal import Decimal
-from django.conf import settings
+# from decimal import Decimal
+# from django.conf import settings
+from django.shortcuts import get_object_or_404
+from adventures.models import Adventure
 
 def bag_contents(request):
     """ A method to work out the shopping bag total """
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Adventure, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
