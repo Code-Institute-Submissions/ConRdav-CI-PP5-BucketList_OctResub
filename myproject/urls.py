5755ad332beba6dfd51d0d013377916c359f4650
ConRdav <connor@redwoodglade.com>
2022-09-01 14:10:37 +0000

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('adventures/', include('adventures.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('testimonials/', include('testimonials.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
