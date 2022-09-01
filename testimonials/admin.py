00b6226b4ce0a6bae53c96e556303fa72ddf7da7
ConRdav <connor@redwoodglade.com>
2022-09-01 14:37:11 +0000

    Update testimonials admin.py

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Testimonial
 
 # Register your models here.
+
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = (
+        'country',
+        'user',
+        'pub_date',
+        'comment',
+        'value',
+    )
+
+admin.site.register(Testimonial, TestimonialAdmin)
