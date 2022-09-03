5a4b0bc9e724b9c4fd97955115460a44eb5194d6
ConRdav <connor@redwoodglade.com>
2022-09-03 09:37:40 +0000

    Update bag template and adventure detail template

--- adventures/urls.py
+++ adventures/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_adventures, name='adventures'),
-    path('<adventure_id>', views.adventure_detail, name='adventure_detail'),
-    path('country_id', views.excursion_detail, name='excursion_detail'),
+    path('<adventure_id>/', views.adventure_detail, name='adventure_detail'),
+    path('<country_id>/', views.excursion_detail, name='excursion_detail'),
 ]
