6f1d806c12c6e5bbbd3f6823ebb07a2667b487d6
ConRdav <connor@redwoodglade.com>
2022-09-04 22:11:11 +0000

    add stripe elements js and css

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LeQcKJg5lsH5J6iibutuIijInoboyrrztWhMiQSev9UGFsaoy2uSqNWZLEAAh4IrSpGfvyxR1X4nmCtG2ssLiiu00umzLBS0h',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
