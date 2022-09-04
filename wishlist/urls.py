e8cef08cdbac0da1109cdf5bcca7e298f435dbd8
ConRdav <connor@redwoodglade.com>
2022-09-04 13:41:42 +0000

    Update adventure_detail for wishlist action and update wishlist urls and views

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wishlist, name='wishlist'),
+    path('', views.view_wishlist, name='wishlist'),
+    path('add/<item_id>/', views.add_to_wishlist, name='add_to_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
-def wishlist(request):
+def view_wishlist(request):
 
     return render(request, 'wishlist/wishlist.html')
+
+
+def add_to_wishlist(request, item_id):
+    """ Add an adventure to the wishlist """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    wishlist = request.session.get('wishlist', {})
+
+    if item_id in list(wishlist.keys()):
+        wishlist[item_id] = quantity
+   
+    request.session['wishlist'] = wishlist
+    return redirect(redirect_url)
