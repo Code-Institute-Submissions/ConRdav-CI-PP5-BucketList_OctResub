b2143a36c4c1bb4ee8f9894cef810c845e55c84a
ConRdav <connor@redwoodglade.com>
2022-09-04 11:45:04 +0000

    Update contact views and template. Update main-nav.html to include testimonials and contact nav

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +54,15 @@ INSTALLED_APPS = [
     # Addons
     'crispy_forms',
 ]
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
