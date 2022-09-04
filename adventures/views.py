b5eb374656f55858ea5a9d0ea6f4f9c367ec7f11
ConRdav <connor@redwoodglade.com>
2022-09-04 14:56:15 +0000

    Update adventure_detail and excursion_detail view

--- adventures/views.py
+++ adventures/views.py
@@ -28,6 +28,7 @@ def adventure_detail(request, adventure_id):
     """ A view to show adventure details """
 
     adventure = get_object_or_404(Adventure, pk=adventure_id)
+    request.session['adventure_id'] = adventure.id
 
     context = {
         'adventure': adventure,
@@ -41,9 +42,11 @@ def excursion_detail(request, country):
 
     excursions = Excursion.objects.all()
     excursions = excursions.filter(country__name__contains=country)
+    adventure_id = request.session.get('adventure_id', None)
 
     context = {
-        'excursions': excursions
+        'excursions': excursions,
+        'adventure_id': adventure_id
     }
 
     return render(request, 'adventures/excursion_detail.html', context)
