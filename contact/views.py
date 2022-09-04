8f83841a2aa002267f2de4f6624ef42742537b02
ConRdav <connor@redwoodglade.com>
2022-09-04 10:45:41 +0000

    Update contact.html and contact views

--- contact/views.py
+++ contact/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from .forms import ContactForm
 
 
-def contact_view(request):
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'contact/success.html')
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
