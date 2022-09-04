6c10a5628d39e86e22896212cbfdee6c4ca583e8
ConRdav <connor@redwoodglade.com>
2022-09-04 23:37:37 +0000

    Update checkout forms, template and view for stripe basic functionality

--- checkout/views.py
+++ checkout/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
-from .forms import OrderForm
 from bag.contexts import bag_contents
+from .forms import OrderForm
 
 import stripe
 
@@ -23,20 +23,20 @@ def checkout(request):
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
+        currency=settings.STRIPE_CURRENCY,
     )
-    
+
     order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
-             Did you forget to set it in your enviroment?')
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'stripe_public_key',
-        'client_secret': 'intent.client_Secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
