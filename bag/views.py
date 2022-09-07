88a6e92ac938ea46dc4cd05d7d88a2f25aac3cda
ConRdav <connor@redwoodglade.com>
2022-09-07 08:46:25 +0000

    update messages in base template, bag views and adventure views

--- bag/views.py
+++ bag/views.py
@@ -22,6 +22,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.error(request, f'Updated {adventure.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.error(request, f'Added {adventure.name} to your bag')
