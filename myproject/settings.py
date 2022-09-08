99c7e9765580adca67cbb4c1b3eaf28521c7cdb6
ConRdav <connor@redwoodglade.com>
2022-09-08 22:12:46 +0000

    completed webhook handlers

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ci-pp5-bucketlist.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['ci-pp5-bucketlist.herokuapp.com', 'localhost']
 
 
 # Application definition
