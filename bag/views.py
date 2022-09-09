42eb49790d6a9fa9d2bc38113bb71cabdd36935e
ConRdav <connor@redwoodglade.com>
2022-09-09 00:09:40 +0000

    Update toast messages css

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from adventures.models import Adventure
@@ -34,13 +34,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the specified adventure to the shopping bag """
 
+    adventure = get_object_or_404(Adventure, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {adventure.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {adventure.name} quantity to {bag[item_id]}')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -50,12 +53,15 @@ def remove_from_bag(request, item_id):
     """ Remove the specified adventure from the shopping bag """
 
     try:
+        adventure = get_object_or_404(Adventure, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed {adventure.name} adventure from your bag.')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing adventure: {e}')
         return HttpResponse(Status=500)
