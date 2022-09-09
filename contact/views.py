7301473f5586e563cdba7a7f65361ea685a53c2c
ConRdav <connor@redwoodglade.com>
2022-09-09 15:13:54 +0000

    Update contact views

--- contact/views.py
+++ contact/views.py
@@ -11,10 +11,6 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            email_subject = f'New contact {form.cleaned_data["email"]}: {form.cleaned_data["subject"]}'
-            email_message = form.cleaned_data['message']
-            send_mail(email_subject, email_message, [settings.EMAIL_HOST_USER],
-                      [settings.EMAIL_HOST_USER])
             messages.success(request, 'Thank you for your enquiry!')
             return redirect('contact')
         else:
