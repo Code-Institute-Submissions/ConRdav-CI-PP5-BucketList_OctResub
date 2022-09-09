39890ac7932b322db620d78c5fdc1612b38939f6
ConRdav <connor@redwoodglade.com>
2022-09-09 13:25:18 +0000

    Update contact admin and views

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactForm
 
 
 def contact(request):
-
-    form = ContactForm()
+    """ Sends a enquiry to the admin """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for your enquiry the we will get back to you asap!')
+            return redirect('contact')
+        else:
+            messages.error(request, 'Failed to send enquiry. Please ensure the form is valid.')
+    else:
+        form = ContactForm()
+     
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
