574a59d5fe2f9bcee3478c6dd666dd4f7664ef6f
ConRdav <connor@redwoodglade.com>
2022-10-10 08:04:58 +0000

    Fix closing bracket in checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,3 @@
-import stripe
-import json
-
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
 )
@@ -8,13 +5,15 @@ from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
-from .forms import OrderForm
-from .models import Order, OrderLineItem
+import stripe
+import json
 
 from adventures.models import Adventure
+from bag.contexts import bag_contents
+from .forms import OrderForm
+from .models import Order, OrderLineItem
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
-from bag.contexts import bag_contents
 
 
 @require_POST
@@ -56,7 +55,7 @@ def checkout(request):
         }
 
         order_form = OrderForm(form_data)
-    
+
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
@@ -76,24 +75,24 @@ def checkout(request):
 
                 except Adventure.DoesNotExist:
                     messages.error(request, (
-                        (
-                            "One of the adventure in your bag wasn't found in our database."
-                            )
-                        ("Please call us for assistance!")
-                    )
+                        "One of the tours in your basket "
+                        "wasn't found in our database."
+                        " Please call us for assistance!"))
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                            args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(request,
+                           "There's nothing in your bag at the moment")
             return redirect(reverse('adventures'))
 
         current_bag = bag_contents(request)
@@ -105,7 +104,6 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
